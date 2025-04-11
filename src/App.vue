e95dab491a5a80d81431e3f4b5729a5e63b56e32
kechangbai <changbai.ke@unifun.tech>
2025-04-11 20:56:10 +0800

    moblie css

--- src/App.vue
+++ src/App.vue
@@ -23,5 +23,27 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
 }
+
+ 
+html,
+body {
+  width: 100%;
+  max-width: 100%; /* 再次确认 */
+  overflow-x: hidden;
+  overscroll-behavior-x: contain; /* 推荐 */
+  margin: 0;
+  padding: 0;
+}
+
+/* 可选，如果需要更强硬禁止拖动手势 */
+/* body, #app {
+  touch-action: pan-y;
+} */
+
+* {
+  box-sizing: border-box;
+}
+
 </style>
